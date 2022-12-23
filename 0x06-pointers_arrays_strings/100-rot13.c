62ff4c2d58c2ebc44b27a392089bf95adfd29f89
israel butdam <israelxinobutam@gmail.com>
2022-12-22 20:41:50 -0800

    strings pointers ♻️💫🔥

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - does something
+ * @c: param
+ * Return: char
+ */
+char *rot13(char *c)
+{
+	int i = 0;
+
+	while (c[i])
+	{
+		while ((c[i] >= 'a' && c[i] <= 'z') || (c[i] >= 'A' && c[i] <= 'Z'))
+		{
+			if ((c[i] > 'm' && c[i] <= 'z') || (c[i] > 'M' && c[i] <= 'Z'))
+			{
+				c[i] -= 13;
+				break;
+			}
+			c[i] += 13;
+			break;
+		}
+		i += 1;
+	}
+
+	return (c);
+}
